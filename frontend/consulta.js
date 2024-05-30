7ad446c88ad257d35138c67fc0f3ada7bb0237e6
Francisco Daniel Gregorio Guevara <160035631+FRANCISCODANIEL03@users.noreply.github.com>
2024-05-30 09:24:57 -0600

    Add files via upload

--- frontend/consulta.js
+++ frontend/consulta.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", async ()=>{
+document.addEventListener("DOMContentLoaded", async () => {
     var id2 = document.getElementById("input_id");
     var nombre2 = document.getElementById("input_nom");
     const insertar = document.getElementById("btn_ins");
@@ -8,87 +8,129 @@ document.addEventListener("DOMContentLoaded", async ()=>{
     const tbl = document.getElementById("tbl_body");
     const tabla = document.getElementById("div_tbl");
     const btn_ocult = document.getElementById("ocult");
-    const alert = document.getElementById("aviso");
-    const URL = "https://api-topicos-1.onrender.com/";
+    const URL = "https://api-topicos-1.onrender.com";
     const limpiarTabla = () => {
         while (tbl.firstChild) {
             tbl.removeChild(tbl.firstChild);
         }
     }
-    const extraerDat= async()=>{
+    const extraerDat = async () => {
         const datos = await fetch(URL);
         const datos2 = await datos.json();
         limpiarTabla();
-        for(let i=0;i < datos2.length;i++){
+        for (let i = 0; i < datos2.length; i++) {
             var tr = document.createElement("tr");
-            var th1 = document.createElement("td");
-            var th2 = document.createElement("td");
-            th1.textContent = datos2[i].id;
-            th2.textContent = datos2[i].nombre;
-            tr.appendChild(th1);
-            tr.appendChild(th2);
+            var td1 = document.createElement("td");
+            var td2 = document.createElement("td");
+            td1.textContent = datos2[i].id;
+            td2.textContent = datos2[i].nombre;
+            td1.classList.add("td-custom");
+            tr.appendChild(td1);
+            tr.appendChild(td2);
             tbl.appendChild(tr);
         }
     }
-    btn_ocult.addEventListener("click",(e)=>{
+    btn_ocult.addEventListener("click", (e) => {
         e.preventDefault();
         tabla.style.display = "none";
     });
-    insertar.addEventListener("click",async (e)=>{
+    insertar.addEventListener("click", async (e) => {
         e.preventDefault();
-       var lista ={
-        nombre : nombre2.value
-       };
-       const lista2= JSON.stringify(lista)
-        const option={
+        var lista = {
+            nombre: nombre2.value
+        };
+        const lista2 = JSON.stringify(lista)
+        const option = {
             method: "POST",
-            headers:{
+            headers: {
                 "Content-type": "application/json"
             },
             body: lista2
         }
-        const res= await fetch(URL,option)
-        const respuesta= await res.json()
-        alert.textContent= respuesta.message;
+        const res = await fetch(URL, option)
+        const respuesta = await res.json()
+        if (respuesta.value == 1) {
+            icon2 = 'success'
+        } else {
+            icon2 = 'warning'
+        }
+        Swal.fire({
+            title: respuesta.message,
+            icon: icon2,
+            confirmButtonText: 'OK',
+            customClass: {
+                title: 'swal2-title-custom',
+                popup: 'swal2-box-shadow',
+                confirmButton: 'swal2-confirm-button-custom'
+            }
+        });
     });
-    consultar.addEventListener("click",async (e)=>{
+    consultar.addEventListener("click", async (e) => {
         e.preventDefault();
         tabla.style.display = "block";
         extraerDat();
     });
-    actualizar.addEventListener("click",async (e)=>{
+    actualizar.addEventListener("click", async (e) => {
         e.preventDefault();
-       var lista ={
-        id : id2.value,
-        nombre : nombre2.value
-       };
-       const lista2= JSON.stringify(lista)
-        const option={
+        var lista = {
+            id: id2.value,
+            nombre: nombre2.value
+        };
+        const lista2 = JSON.stringify(lista)
+        const option = {
             method: "PATCH",
-            headers:{
+            headers: {
                 "Content-type": "application/json"
             },
             body: lista2
         }
-        const res= await fetch(URL,option);
-        const respuesta= await res.json();
-        alert.textContent= respuesta.message;
+        const res = await fetch(URL, option);
+        const respuesta = await res.json();
+        if (respuesta.value == 1) {
+            icon2 = 'success'
+        } else {
+            icon2 = 'warning'
+        }
+        Swal.fire({
+            title: respuesta.message,
+            icon: icon2,
+            confirmButtonText: 'OK',
+            customClass: {
+                title: 'swal2-title-custom',
+                popup: 'swal2-box-shadow',
+                confirmButton: 'swal2-confirm-button-custom'
+            }
+        });
     });
-    eliminar.addEventListener("click",async (e)=>{
+    eliminar.addEventListener("click", async (e) => {
         e.preventDefault();
-        var lista ={
-         id : id2.value
+        var lista = {
+            id: id2.value
         };
-        const lista2= JSON.stringify(lista)
-         const option={
-             method: "DELETE",
-             headers:{
-                 "Content-type": "application/json"
-             },
-             body: lista2
-         }
-         const res= await fetch(URL,option);
-         const respuesta= await res.json();
-         alert.textContent= respuesta.message;
+        const lista2 = JSON.stringify(lista)
+        const option = {
+            method: "DELETE",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: lista2
+        }
+        const res = await fetch(URL, option);
+        const respuesta = await res.json();
+        if (respuesta.value == 1) {
+            icon2 = 'success'
+        } else {
+            icon2 = 'warning'
+        }
+        Swal.fire({
+            title: respuesta.message,
+            icon: icon2,
+            confirmButtonText: 'OK',
+            customClass: {
+                title: 'swal2-title-custom',
+                popup: 'swal2-box-shadow',
+                confirmButton: 'swal2-confirm-button-custom'
+            }
+        });
     });
 });
